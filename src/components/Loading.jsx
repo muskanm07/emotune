6813d8e5712b11d17b8f9fc7b61b88672c48c432
Muskan Maurya <muskanmaurya553@gmail.com>
2026-08-19 21:48:52 +0530

    UI Redesign

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ export default function Loading(){
     return(
 
         <div className="flex justify-center items-center m-12">
-            <div className="h-12 w-12 rounded-full border-6 border-purple-400 border-t-transparent animate-spin"></div>
+            <div className="h-12 w-12 rounded-full border-[#24243D] border-t-[#FF4FA3] border-r-[#A855F7] animate-spin"></div>
         </div>
          
     )
