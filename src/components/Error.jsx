9093a9c42f132492c9c2af2526a409c3dd86e3ba
Muskan Maurya <muskanmaurya553@gmail.com>
2026-08-19 21:59:18 +0530

    UI Redesign

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -6,6 +6,6 @@ export default function Error({error}){
     return(
        
             // <p className="p-4 text-center text-purple-300 text-lg font-semibold ">{error}</p>
-         <p className="my-6 flex items-center gap-2 rounded-xl border border-[#FF4FA3]/60 bg-[#FF4FA3]/10 px-3 py-2 text-sm text-[#FFB8D9]"><FaExclamationCircle size={14}/>{error}</p>
+         <p className="my-6 w-full md:w-100 flex items-center gap-2 rounded-xl border border-[#FF4FA3]/60 bg-[#FF4FA3]/10 px-3 py-2 text-sm text-[#FFB8D9]"><FaExclamationCircle size={14}/>{error}</p>
     )
 }
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ export default function Loading(){
     return(
 
         <div className="flex justify-center items-center m-12">
-            <div className="h-12 w-12 rounded-full border-[#24243D] border-t-[#FF4FA3] border-r-[#A855F7] animate-spin"></div>
+            <div className="h-8 w-8 rounded-full border-4 border-[#24243D] border-t-[#FF4FA3] border-r-[#A855F7] animate-spin"></div>
         </div>
          
     )
